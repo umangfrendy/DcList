fbb56664c3ff84cd60a3f1610292d11e29294155
umangfrendy <umang.t@frendy.in>
2021-12-06 13:17:37 +0530

    DEC 6 13:17  v-expansion-panels

--- src/components/DcList.vue
+++ src/components/DcList.vue
@@ -1,22 +1,16 @@
 <template>
   <div>
-    <v-app-bar
-      color="deep-purple accent-4"
-      dense
-      dark
-    >
+    <v-app-bar color="deep-purple accent-4" dense dark>
       <v-app-bar-nav-icon>
-          <!-- <v-list-item-title></v-list-item-title> -->
+        <!-- <v-list-item-title></v-list-item-title> -->
       </v-app-bar-nav-icon>
       <v-btn icon>
         <v-icon>mdi-chevron-left</v-icon>
       </v-btn>
 
-
       <v-toolbar-title>DC List</v-toolbar-title>
 
       <v-spacer></v-spacer>
-      
 
       <!-- <v-menu
         left
@@ -44,24 +38,38 @@
       </v-menu> -->
     </v-app-bar>
 
-     <div class="font-weight-bold subtitle pa-2">Delivery Chalan Details</div>
-     
+    <div class="font-weight-bold subtitle pa-2">Delivery Chalan Details</div>
+
     <div align="center">
-    <h3>Delivery Otp:XXXX</h3>
-    <div>
+      <h3>Delivery Otp:XXXX</h3>
+      <div>
+        <v-tabs center-active>
+          <v-tab class="font-weight-bold"> Upcoming</v-tab>
+          <v-tab class="font-weight-bold"> Delivered</v-tab>
+        </v-tabs>
 
-      <v-tabs v-model="tab1" center-active>
-        <v-tab  class="font-weight-bold">
-          Upcoming</v-tab
-        >
-        <v-tab class="font-weight-bold" >
-          Delivered</v-tab
-        >
-      </v-tabs>
+        <div>
+          <template>
+            <div>
+              <v-expansion-panels class="mb-6">
+                <v-expansion-panel v-for="(item, i) in 5" :key="i">
+                  <v-expansion-panel-header expand-icon="mdi-chevron-down">
+                    Item
+                  </v-expansion-panel-header>
+                  <v-expansion-panel-content
+                    >Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat.</v-expansion-panel-content
+                  >
+                </v-expansion-panel>
+              </v-expansion-panels>
 
+            </div>
+          </template>
+        </div>
+      </div>
     </div>
   </div>
-
-  </div>
-  
 </template>
